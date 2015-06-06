aeec9c107c4a13f4b952723cdc202410c02f58a3
Rupert Bailey <rupert@learningmethod.com.au>
2015-06-06 18:41:23 +1000

    missed a file

--- do-it.sh
+++ do-it.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #ssh hetzner "mkdir -p ~/projects/nab/git/sql"
-scp *.sql hetzner:~/projects/nab/git/sql/
+scp *.sql run_sql.sh hetzner:~/projects/nab/git/sql/
 ssh hetzner '~/projects/nab/git/scripts/run_sql.sh'
